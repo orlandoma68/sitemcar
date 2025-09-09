c2104691065008d94bf6ba7e06f736955d400d50
orlan001 <orlandoma68@gmail.com>
2025-09-09 20:48:22 -0300

    detail

--- src/components/Itemdetail.jsx
+++ src/components/Itemdetail.jsx
@@ -13,13 +13,13 @@ const Itemdetail = ({item}) => {
     <>
         {
             item && 
-            <div className='d-flex justify-content-center align-items-center min-vh-100'>
-                <div className="d-flex justify-content-center align-items-center" style={ {width: 50 +'rem'}}>                    
-                        <div className='bg-light d-flex justify-content-center align-items-center m-3 p-4' style={ {height: 280 +'px'}  }>
+            <div className='container d-flex justify-content-center align-items-center min-vh-100'>
+                <div className="row d-flex justify-content-center align-items-center" style={ {width: 50 +'rem'}}>                    
+                        <div className='col-md-6 bg-light d-flex justify-content-center align-items-center m-3 p-4' style={ {height: 280 +'px'}  }>
                             <img src={item.img} alt={item.nombre} className="img-fluid w-100" style={{height: 280 +'px', objectFit:'cover'}  }/>
                         </div>
                         
-                        <div className='m-3 d-flex flex-column'>
+                        <div className='col m-3 d-flex flex-column'>
                             <h5 className="card-title">{item.nombre}</h5>
                             <h3 className="card-text">{item.categoria}</h3>
                             <p className="card-text">{item.descripcion}</p>
